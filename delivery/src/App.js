976fa9316867cfcfda64a8ded129ddcdf2240b35
Muhammad <muhammadmansurov111@gmail.com>
2021-08-21 20:39:23 +0500

    Login screen WIP

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="left-side">
+          
+        </div>
+        <div className="right-side">
+          <div className='right-inner'> 
+          <h1>Welcome</h1>
+          </div>
+          <div className="inputs">
+            <input type="Email"  placeholder="Email" />
+            <input type="Password" placeholder="Password" />
+          </div>
+          <button>Sign in</button>
+        </div>
     </div>
   );
 }
